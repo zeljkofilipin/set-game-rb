aa95f678d8a8a69468b389d9b20f118a4afd4143
Zeljko Filipin <zeljko.filipin@gmail.com>
2012-07-21 15:45:39 +0200

    refactor: #combinations_for_cards moved to SetGame::Game

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,8 @@
 module SetGame
   class Game
+    def self.combinations_for_cards(cards)
+      cards.combination(3).to_a
+    end
     def strings
       require_relative "browser"
       SetGame::Browser.new.get_cards
--- lib/set.rb
+++ lib/set.rb
@@ -1,6 +1,3 @@
-def combinations_for_cards(cards)
-  cards.combination(3).to_a
-end
 def sets_for_cards(cards)
 end
 def valid_set?(cards)
