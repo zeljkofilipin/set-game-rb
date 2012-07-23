88a17fce6f4759306b2598099c0838b3cad5ff32
Zeljko Filipin <zeljko.filipin@gmail.com>
2012-07-23 11:04:52 +0200

    refactoring: renamed method

--- lib/browser.rb
+++ lib/browser.rb
@@ -4,7 +4,7 @@ module SetGame
       require "watir-webdriver"
       @browser = Watir::Browser.new
     end
-    def get_cards
+    def strings
       @browser.goto "http://thebreretons.com/setgame/"
       @browser.imgs(id: /^card?/).collect {|img| img.alt}
     end
