d86a4cb4d57fb435b95a2f9562b47c523dd5fa93
Zeljko Filipin <zeljko.filipin@gmail.com>
2012-07-23 11:11:10 +0200

    refactoring: use strings instead of arrays to represent cards

--- spec/set_spec.rb
+++ spec/set_spec.rb
@@ -5,52 +5,52 @@ describe SetGame::Set do
     # screenshot of the game is at misc/1.png
     cards =
       [
-        ["two", "red", "empty", "diamonds"],
-        ["three", "green", "empty", "diamonds"],
-        ["one", "purple", "empty", "squiggle"],
-        ["two", "red", "empty", "ovals"],
+        ["two red empty diamonds"],
+        ["three green empty diamonds"],
+        ["one purple empty squiggle"],
+        ["two red empty ovals"],
 
-        ["two", "green", "solid", "ovals"],
-        ["one", "purple", "shaded", "squiggle"],
-        ["one", "purple", "empty", "diamond"],
-        ["two", "red", "shaded", "ovals"],
+        ["two green solid ovals"],
+        ["one purple shaded squiggle"],
+        ["one purple empty diamond"],
+        ["two red shaded ovals"],
 
-        ["two", "green", "shaded", "ovals"],
-        ["one", "purple", "solid", "oval"],
-        ["one", "green", "empty", "squiggle"],
-        ["three", "green", "solid", "diamonds"]
+        ["two green shaded ovals"],
+        ["one purple solid oval"],
+        ["one green empty squiggle"],
+        ["three green solid diamonds"]
       ]
     sets =
       [
         [
-          ["two", "red", "empty", "diamonds"],
-          ["three", "green", "empty", "diamonds"],
-          ["one", "purple", "empty", "diamond"],
+          ["two red empty diamonds"],
+          ["three green empty diamonds"],
+          ["one purple empty diamond"],
         ],
         [
-          ["one", "purple", "solid", "oval"],
-          ["one", "purple", "shaded", "squiggle"],
-          ["one", "purple", "empty", "diamond"],
+          ["one purple solid oval"],
+          ["one purple shaded squiggle"],
+          ["one purple empty diamond"],
         ],
         [
-          ["one", "green", "empty", "squiggle"],
-          ["three", "green", "solid", "diamonds"],
-          ["two", "red", "shaded", "ovals"],
+          ["one green empty squiggle"],
+          ["three green solid diamonds"],
+          ["two red shaded ovals"],
         ],
         [
-          ["three", "green", "solid", "diamonds"],
-          ["two", "red", "empty", "ovals"],
-          ["one", "purple", "shaded", "squiggle"],
+          ["three green solid diamonds"],
+          ["two red empty ovals"],
+          ["one purple shaded squiggle"],
         ],
         [
-          ["three", "green", "empty", "diamonds"],
-          ["one", "purple", "empty", "squiggle"],
-          ["two", "red", "empty", "ovals"],
+          ["three green empty diamonds"],
+          ["one purple empty squiggle"],
+          ["two red empty ovals"],
         ],
         [
-          ["three", "green", "solid", "diamonds"],
-          ["one", "purple", "empty", "squiggle"],
-          ["two", "red", "shaded", "ovals"],
+          ["three green solid diamonds"],
+          ["one purple empty squiggle"],
+          ["two red shaded ovals"],
         ],
       ]
 
