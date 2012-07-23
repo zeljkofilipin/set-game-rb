add80f557aac0890a8d3a06068831fc99dba8bcf
Zeljko Filipin <zeljko.filipin@gmail.com>
2012-07-23 10:45:21 +0200

    refactor: move functionality from game to card class

--- lib/card.rb
+++ lib/card.rb
@@ -4,5 +4,10 @@ module SetGame
       features = string.split " "
       {count: features[0], color: features[1], fill: features[2], shape: features[3]}
     end
+    def self.cards(strings)
+      strings.collect do |string|
+        card(string)
+      end
+    end
   end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -5,4 +5,38 @@ describe SetGame::Card do
     card = {:count=>"three", :color=>"purple", :fill=>"solid", :shape=>"squiggles"}
     SetGame::Card.card(string).should == card
   end
+  it "creates cards from strings" do
+    strings =
+      [
+        "three purple solid squiggles",
+        "two green shaded diamonds",
+        "one red solid squiggle",
+        "three green empty squiggles",
+        "three red solid diamonds",
+        "two purple shaded squiggles",
+        "two red empty diamonds",
+        "one purple empty squiggle",
+        "three purple shaded ovals",
+        "three red shaded ovals",
+        "one green shaded oval",
+        "one purple empty oval"
+      ]
+    cards =
+      [
+        {:count=>"three", :color=>"purple", :fill=>"solid", :shape=>"squiggles"},
+        {:count=>"two", :color=>"green", :fill=>"shaded", :shape=>"diamonds"},
+        {:count=>"one", :color=>"red", :fill=>"solid", :shape=>"squiggle"},
+        {:count=>"three", :color=>"green", :fill=>"empty", :shape=>"squiggles"},
+        {:count=>"three", :color=>"red", :fill=>"solid", :shape=>"diamonds"},
+        {:count=>"two", :color=>"purple", :fill=>"shaded", :shape=>"squiggles"},
+        {:count=>"two", :color=>"red", :fill=>"empty", :shape=>"diamonds"},
+        {:count=>"one", :color=>"purple", :fill=>"empty", :shape=>"squiggle"},
+        {:count=>"three", :color=>"purple", :fill=>"shaded", :shape=>"ovals"},
+        {:count=>"three", :color=>"red", :fill=>"shaded", :shape=>"ovals"},
+        {:count=>"one", :color=>"green", :fill=>"shaded", :shape=>"oval"},
+        {:count=>"one", :color=>"purple", :fill=>"empty", :shape=>"oval"}
+      ]
+
+    SetGame::Card.cards(strings).should == cards
+  end
 end
