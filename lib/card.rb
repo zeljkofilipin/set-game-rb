92c2228c8c7cd663aed3d2f4f01b4eed3fe45656
Željko Filipin <zeljko.filipin@gmail.com>
2016-06-11 12:53:15 +0200

    fixed Style/EmptyLineBetweenDefs rubocop offense

--- lib/card.rb
+++ lib/card.rb
@@ -4,11 +4,13 @@ module SetGame
       features = string.split " "
       {count: features[0], color: features[1], fill: features[2], shape: features[3]}
     end
+
     def self.cards(strings)
       strings.collect do |string|
         card(string)
       end
     end
+
     def self.combinations(strings)
       cards = strings.each { |string| string.split " "}
       cards.combination(3).to_a
--- lib/set.rb
+++ lib/set.rb
@@ -5,14 +5,17 @@ module SetGame
       cards = SetGame::Card.cards(strings)
       cards.collect { |card| card[feature].chomp("s")}.uniq.size == 3
     end
+
     def self.feature_the_same_for_all_cards(feature, strings)
       require_relative "card"
       cards = SetGame::Card.cards(strings)
       cards.collect { |card| card[feature].chomp("s")}.uniq.size == 1
     end
+
     def self.feature_the_same_or_different_for_all_cards?(feature, strings)
       feature_the_same_for_all_cards(feature, strings) || feature_different_for_all_cards(feature, strings)
     end
+
     def self.sets(cards)
       require_relative "card"
       combinations = SetGame::Card.combinations(cards)
@@ -20,6 +23,7 @@ module SetGame
         set if valid?(set)
       end.compact
     end
+
     def self.valid?(cards)
       features = [:count, :color, :fill, :shape]
       valid = features.collect do |feature|
