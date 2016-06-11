971ef59baaacd9f71786acc63198407994f4a507
Željko Filipin <zeljko.filipin@gmail.com>
2016-06-11 13:43:12 +0200

    moved opening and closing the browser to before and after rspec hooks

--- spec/selenium_spec.rb
+++ spec/selenium_spec.rb
@@ -1,12 +1,15 @@
 describe 'Set game' do
-  it 'opens the game' do
+  before(:each) do
     require 'selenium-webdriver'
+    @driver = Selenium::WebDriver.for :firefox
+    @driver.navigate.to 'http://smart-games.org/en/set_classic/start/10'
+  end
 
-    driver = Selenium::WebDriver.for :firefox
-    driver.navigate.to 'http://smart-games.org/en/set_classic/start/10'
+  after(:each) do
+    @driver.quit
+  end
 
-    expect(driver.title).to eq('Set - online card game')
-
-    driver.quit
+  it 'opens the game' do
+    expect(@driver.title).to eq('Set - online card game')
   end
 end
