bc9445bea0170985391939576307c5963b914c09
Željko Filipin <zeljko.filipin@gmail.com>
2016-06-11 13:22:51 +0200

    fixed Metrics/LineLength rubocop offense

--- spec/set_spec.rb
+++ spec/set_spec.rb
@@ -65,9 +65,12 @@ describe SetGame::Set do
         'three purple solid squiggles',
         'three purple solid squiggles'
       ]
-    expect(SetGame::Set.feature_the_same_for_all_cards(:count, cards)).to eq(false)
-    expect(SetGame::Set.feature_the_same_for_all_cards(:color, cards)).to eq(true)
-    expect(SetGame::Set.feature_the_same_for_all_cards(:shape, cards)).to eq(true)
+    expect(SetGame::Set.feature_the_same_for_all_cards(:count, cards))
+      .to eq(false)
+    expect(SetGame::Set.feature_the_same_for_all_cards(:color, cards))
+      .to eq(true)
+    expect(SetGame::Set.feature_the_same_for_all_cards(:shape, cards))
+      .to eq(true)
   end
   it 'knows if one feature of a set is different for all cards' do
     cards =
@@ -76,9 +79,12 @@ describe SetGame::Set do
         'two purple solid squiggles',
         'three purple solid squiggles'
       ]
-    expect(SetGame::Set.feature_different_for_all_cards(:count, cards)).to eq(true)
-    expect(SetGame::Set.feature_different_for_all_cards(:color, cards)).to eq(false)
-    expect(SetGame::Set.feature_different_for_all_cards(:shape, cards)).to eq(false)
+    expect(SetGame::Set.feature_different_for_all_cards(:count, cards))
+      .to eq(true)
+    expect(SetGame::Set.feature_different_for_all_cards(:color, cards))
+      .to eq(false)
+    expect(SetGame::Set.feature_different_for_all_cards(:shape, cards))
+      .to eq(false)
   end
   it 'knows if one feature of a set is either the same or different for all'\
      ' cards' do
@@ -88,11 +94,11 @@ describe SetGame::Set do
         'two purple solid squiggles',
         'three purple solid squiggles'
       ]
-    expect(SetGame::Set.feature_the_same_or_different_for_all_cards?(:count, cards))
-      .to eq(true)
-    expect(SetGame::Set.feature_the_same_or_different_for_all_cards?(:color, cards))
-      .to eq(false)
-    expect(SetGame::Set.feature_the_same_or_different_for_all_cards?(:fill, cards))
-      .to eq(true)
+    expect(SetGame::Set
+      .feature_the_same_or_different_for_all_cards?(:count, cards)).to eq(true)
+    expect(SetGame::Set
+      .feature_the_same_or_different_for_all_cards?(:color, cards)).to eq(false)
+    expect(SetGame::Set
+      .feature_the_same_or_different_for_all_cards?(:fill, cards)).to eq(true)
   end
 end
